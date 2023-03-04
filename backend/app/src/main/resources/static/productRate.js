1f995e3e1bb7d27a546ce9b20b84ae9df471c381
Inés <i.alonso.2020@alumnos.urjc.es>
2023-03-04 20:45:31 +0100

    product rating stars
    now the average of the value of the reviews is generated and is displayed in the overall value of the product

--- backend/app/src/main/resources/static/productRate.js
+++ backend/app/src/main/resources/static/productRate.js
@@ -1,12 +1,12 @@
 function rate(){
 
     let img =document.getElementById("starRating");
-    let imgProduct = document.getElementById("productRating");
-    let int =document.getElementById("rate").textContent;
-    let int2 =document.getElementById("productRate").textContent;
+    let int =document.getElementById("rate");
 
-    img.src="/"+ int + ".png"
-    imgProduct.src="/"+ int2 + ".png"
+    if (int!=null){
+        img.src="/"+ int.textContent + ".png"
+    }
+
 }
 
 rate();
