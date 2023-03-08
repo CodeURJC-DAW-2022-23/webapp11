9938af6b77574ac99498a8991a2bbb78bf172254
Santiago <santi.ari.pani@protonmail.com>
2023-03-08 11:54:03 +0100

    API Security
    Implemented the JWT and adapted our Rest Security Config to be compatible with our version of Spring and Spring Security.
    
    @Andreas4122002  the Image controller doesn't work btw

---
+++ backend/app/src/main/java/com/techmarket/app/security/RestSecurityConfiguration.java
@@ -0,0 +1,79 @@
+package com.techmarket.app.security;
+
+import com.techmarket.app.security.jwt.JwtRequestFilter;
+import com.techmarket.app.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@Order(1)
+public class RestSecurityConfiguration extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>
+{
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+
+    //Expose AuthenticationManager as a Bean to be used in other services
+    //@Bean
+    //@Override
+    //public AuthenticationManager authenticationManagerBean() throws Exception {
+    //    return super.authenticationManagerBean();
+    //}
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userService);
+        authProvider.setPasswordEncoder(passwordEncoder);
+
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration) throws Exception {
+        return authConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public FilterRegistrationBean<JwtRequestFilter> jwtRequestFilterRegistration() {
+        FilterRegistrationBean<JwtRequestFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(jwtRequestFilter);
+        registration.addUrlPatterns("/api/*");
+        return registration;
+    }
+    @Override
+    public void configure(HttpSecurity http) throws Exception
+    {
+        http
+            .authorizeHttpRequests()
+            .requestMatchers("/api/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .csrf().disable()
+            .httpBasic().disable()
+            .formLogin().disable();
+            http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
