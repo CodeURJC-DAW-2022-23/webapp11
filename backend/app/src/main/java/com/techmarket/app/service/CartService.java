b8c81304cd7180c419eec7c210b8d8fa6aa1c3b2
Andreas4122002 <79724220+Andreas4122002@users.noreply.github.com>
2023-03-13 11:43:29 +0100

    Cart api controller

--- backend/app/src/main/java/com/techmarket/app/service/CartService.java
+++ backend/app/src/main/java/com/techmarket/app/service/CartService.java
@@ -2,8 +2,16 @@ package com.techmarket.app.service;
 
 
 import com.techmarket.app.Repositories.ProductRepository;
+import com.techmarket.app.model.Product;
+import com.techmarket.app.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CartService {
@@ -11,6 +19,12 @@ public class CartService {
     @Autowired
     private ProductRepository productRepository;
 
+    public Page<Product> getCartProducts(Pageable pageable, User user) {
+        PageRequest pageRequest = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize());
+        List<Product> cartProducts = user.getShoppingCart();
 
+
+        return new PageImpl<>(cartProducts, pageRequest, cartProducts.size());
+    }
 
 }
