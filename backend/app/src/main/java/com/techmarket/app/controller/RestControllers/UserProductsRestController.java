ca027d6d55f0d666aa8106424a7a2b30b9d6c5e6
Andreas4122002 <79724220+Andreas4122002@users.noreply.github.com>
2023-03-18 21:25:03 +0100

    Recommendation API Controller
    API now returns the 4 recommended products for the user in the main page plus some minor fixes to the cart and wishlist so it returns a success or error message when executed

--- backend/app/src/main/java/com/techmarket/app/controller/RestControllers/UserProductsRestController.java
+++ backend/app/src/main/java/com/techmarket/app/controller/RestControllers/UserProductsRestController.java
@@ -3,7 +3,9 @@ package com.techmarket.app.controller.RestControllers;
 
 import com.techmarket.app.model.Product;
 import com.techmarket.app.model.User;
+import com.techmarket.app.security.jwt.AuthResponse;
 import com.techmarket.app.service.ProductService;
+import com.techmarket.app.service.RecommendationService;
 import com.techmarket.app.service.UserProductsService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -17,6 +19,7 @@ import com.techmarket.app.service.UserService;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -30,6 +33,9 @@ public class UserProductsRestController{
     private UserProductsService userProductsService;
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private RecommendationService recommendationService;
 
     @GetMapping(value="/cart" ,params = {"page", "size"})
     public ResponseEntity<Page<Product>> getCartProducts(@RequestParam(defaultValue = "0") int page,
@@ -56,37 +62,47 @@ public class UserProductsRestController{
 
 
     @PostMapping(value="/cart/addProduct/{id}")
-    public ResponseEntity<Void> addProductToCart(@PathVariable Long id,HttpServletRequest request) {
+    public ResponseEntity<AuthResponse> addProductToCart(@PathVariable Long id,HttpServletRequest request) {
        User user = userService.getCurrentUser(request);
        user.getShoppingCart().add(productService.getProductById(id));
        userService.saveUser(user);
-        return ResponseEntity.noContent().build();
+        AuthResponse authResponse = new AuthResponse(AuthResponse.Status.SUCCESS, "Product added to cart");
+        return ResponseEntity.ok(authResponse);
 
     }
 
    @DeleteMapping(value="/cart/removeProduct/{id}")
-    public ResponseEntity<Void> removeProductFromCart(@PathVariable Long id, HttpServletRequest request) {
+    public ResponseEntity<AuthResponse> removeProductFromCart(@PathVariable Long id, HttpServletRequest request) {
         User user = userService.getCurrentUser(request);
         user.getShoppingCart().remove(productService.getProductById(id));
        userService.saveUser(user);
-        return ResponseEntity.noContent().build();
+        AuthResponse authResponse = new AuthResponse(AuthResponse.Status.SUCCESS, "Product removed from cart");
+        return ResponseEntity.ok(authResponse);
     }
 
     @PostMapping(value="/wishlist/addProduct/{id}")
-    public ResponseEntity<Void> addProductToWishlist(@PathVariable Long id, HttpServletRequest request) {
+    public ResponseEntity<AuthResponse> addProductToWishlist(@PathVariable Long id, HttpServletRequest request) {
         User user  = userService.getCurrentUser(request);
         user.getWishlist().add(productService.getProductById(id));
         userService.saveUser(user);
-        return ResponseEntity.noContent().build();
+        AuthResponse authResponse = new AuthResponse(AuthResponse.Status.SUCCESS, "Product added to wishlist");
+        return ResponseEntity.ok(authResponse);
 
     }
 
     @DeleteMapping(value="/wishlist/removeProduct/{id}")
-    public ResponseEntity<Void> removeProductFromWishlist(@PathVariable Long id, HttpServletRequest request) {
+    public ResponseEntity<AuthResponse> removeProductFromWishlist(@PathVariable Long id, HttpServletRequest request) {
         User user = userService.getCurrentUser(request);
         user.getWishlist().remove(productService.getProductById(id));
-        userService.saveUser(user); 
-        return ResponseEntity.noContent().build();
+        userService.saveUser(user);
+        AuthResponse authResponse = new AuthResponse(AuthResponse.Status.SUCCESS, "Product removed from wishlist");
+        return ResponseEntity.ok(authResponse);
+    }
+
+    @GetMapping("/recommendations")
+    public ResponseEntity<List<Product>> getRecommendations() {
+        List<Product> recommendations = recommendationService.getRecommendedProducts();
+        return ResponseEntity.ok(recommendations);
     }
 
 
