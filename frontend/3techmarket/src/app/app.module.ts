46e67f4bdfa273e6965e429a8461b3a8d9a892f5
Santiago <santi.ari.pani@protonmail.com>
2023-04-04 22:58:31 +0200

    Component refactor
    Components can now be found on their respective folders.
    
    Co-Authored-By: Andreas4122002 <79724220+Andreas4122002@users.noreply.github.com>

--- frontend/3techmarket/src/app/app.module.ts
+++ frontend/3techmarket/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HeaderComponent} from "./header.component";
-import {ItemsComponent} from "./items.component";
-import {FooterComponent} from "./footer.component";
+import {FooterComponent} from "./footer/footer.component";
+import {HeaderComponent} from "./header/header.component";
+import {ItemsComponent} from "./items/items.component";
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
