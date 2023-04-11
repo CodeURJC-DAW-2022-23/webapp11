8c30b6995de958d25c450971d18fdef4f0490675
Santiago <santi.ari.pani@protonmail.com>
2023-04-11 21:37:14 +0200

    Deployment configuration
    Waiting for the project to be done configuring the controllers

--- backend/app/src/main/java/com/techmarket/app/controller/SPAController.java
+++ backend/app/src/main/java/com/techmarket/app/controller/SPAController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class SPAController {
     // Map the /new and /new/** paths to the SPA index.html
     @GetMapping({"/new", "/new/**"})
-    public String index() {
-        return "forward:/new/index.html";
+    public String redirectToSpa() {
+        return "forward:/index.html";
     }
 }
