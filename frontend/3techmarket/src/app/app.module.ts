c068a91b22216551ac7d14cfb91bfda4a70a49d4
Andreas4122002 <79724220+Andreas4122002@users.noreply.github.com>
2023-04-11 12:20:46 +0200

    Product review history
    Still not finished

--- frontend/3techmarket/src/app/app.module.ts
+++ frontend/3techmarket/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { ChatsComponent } from './chats/chats.component';
 import { RecentordersComponent } from './recentorders/recentorders.component';
 import { CartComponent } from './cart/cart.component';
+import { ReviewHistoryComponent } from './reviewHistory/review-history/review-history.component';
 
 // @ts-ignore
 // @ts-ignore
@@ -40,6 +41,7 @@ import { CartComponent } from './cart/cart.component';
     ChatsComponent,
     RecentordersComponent,
     CartComponent,
+    ReviewHistoryComponent,
   ],
   imports: [
     BrowserModule,
---
+++ frontend/3techmarket/src/app/reviewHistory/review-history/review-history.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-history',
+  templateUrl: './review-history.component.html',
+  styleUrls: ['./review-history.component.css']
+})
+export class ReviewHistoryComponent {
+
+}
