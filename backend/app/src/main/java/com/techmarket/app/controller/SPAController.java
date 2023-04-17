7db3cda604b7d22904b888c680bc4d49738e9498
Santiago <santi.ari.pani@protonmail.com>
2023-04-17 12:32:58 +0200

    spa and controller not working

--- backend/app/src/main/java/com/techmarket/app/controller/SPAController.java
+++ backend/app/src/main/java/com/techmarket/app/controller/SPAController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SPAController {
-    // Map the /new and /new/** paths to the SPA index.html
+    // Forward request made to /new and /new/** to /new/index.html
 
     @GetMapping({"/new", "/new/**"})
-    public String index() {
-        return "forward:/index.html";
+    public String spa() {
+        return "forward:/new/index.html";
     }
 }
